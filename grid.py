013f1d0c775b029c3f01f570cba2ab442b044ec7
sakethnadiger <saketh.nadiger@gmail.com>
2025-07-24 19:52:56 +0100

    v1 adjacency list

--- grid.py
+++ grid.py
@@ -36,10 +36,62 @@ class Grid:
         self.gridArray = [[0 for col in range(self.columns)] for row in range(self.rows)]
     
     # generate adjacency list
-    def adjacencyList():
+    def adjacencyList(self):
+        
+        def validEdges(neighbours):
+            edges = {}
+            for n in neighbours:
+                val = self.gridArray[n[1]][n[0]]
+                if val != "#":
+                    edges[n] = val
+            return edges
+        
         adjacency = {}
-
+        for y, row in enumerate(self.gridArray):
+            for x, val in enumerate(row):
+                if val != "#":
+                    # firstly check boundary conditions
+                    # top left - add right and below
+                    if x == 0 and y == 0:
+                        neighbours = [(x+1, y), (x, y+1)]
+                        
+                    # top right - add left and below
+                    elif x == len(row)-1 and y == 0:
+                        neighbours = [(x-1, y), (x, y+1)]
+                        
+                    # top row - add left, right and below
+                    elif y == 0:
+                        neighbours = [(x-1, y), (x+1, y), (x, y+1)]
+                        
+                    # bottom left - add right and above
+                    elif x == 0 and y == len(self.gridArray) - 1:
+                        neighbours = [(x+1, y), (x, y-1)]
+                    
+                    # bottom right - add left and above
+                    elif x == len(row) - 1 and y == len(self.gridArray) - 1:
+                        neighbours = [(x-1, y), (x, y-1)]
+                    
+                    # bottom row - add left, right and above
+                    elif y == len(self.gridArray) - 1:
+                        neighbours = [(x-1, y), (x+1, y), (x, y-1)]
+                    
+                    # left column - add above, right and below
+                    elif x == 0:
+                        neighbours = [(x, y-1), (x+1, y), (x, y+1)]
+                    
+                    # right column - add above, left and below
+                    elif x == len(row) - 1:
+                        neighbours = [(x, y-1), (x-1, y), (x, y+1)]
+                    
+                    # normal conditions - above, below, left, right
+                    else:
+                        neighbours = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
+                    
+                    # add map of weighted neighbours to adjacency list
+                    adjacency[(x, y)] = validEdges(neighbours)
+        
+        return adjacency
+                    
 test = Grid(5, 5)
-
-test.insertValue(8, 0, 0)
-test.outputGrid()
+test.adjacencyList()
+#test.outputGrid()
